12fe0fceb4da047f171ae98e52504cf71668f187
Hanne <hanne273@gmail.com>
2023-05-02 20:06:55 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">jjj</div>;
+  return (
+    <div className="App">
+      <footer>
+        <a
+          href="https://github.com/wecodeschool/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open-sourced project
+        </a>{" "}
+        by Hanne S. Thorud
+      </footer>
+    </div>
+  );
 }
